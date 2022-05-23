ae36fd9d322bd79edb90f77591d0704b3ac4ceef
jeongsam3659 <57051148+jeongsam3659@users.noreply.github.com>
2022-05-24 02:06:08 +0900

    5/24
    자료형 확인차

---
+++ RE_JAVA/src/pj01/sec01/exam04.java
@@ -0,0 +1,21 @@
+package pj01.sec01;
+
+public class exam04 {
+
+	public static void main(String[] args) {
+		String name = "홍길동";
+		String job = "프로그래머";
+		System.out.println(name + " " + job);
+		
+		String str = "나는 \"자바\"를 좋아합니다. ";
+		System.out.println(str);
+
+		String str1 = "나는 \t자바\t를 좋아합니다. ";
+		System.out.println(str1);
+		
+		String str2 = "나는 \n자바\n를 좋아합니다. ";
+		System.out.println(str2);
+		
+	}
+
+}
