a473180bc5f7b069816395f4484b5b7fe4af8759
jeongsam3659 <57051148+jeongsam3659@users.noreply.github.com>
2022-06-14 01:36:07 +0900

    99 100 101 102 103 104

---
+++ RE_JAVA/src/udmy/act9/MyChar.java
@@ -0,0 +1,15 @@
+package udmy.act9;
+
+public class MyChar {
+	private char ch;
+	
+	public MyChar(char ch) {
+		this.ch = ch;
+	}
+	
+	public boolean isVowel() {
+		// 'a' e i o u or A E I O U
+		
+		return false;
+	}
+}
---
+++ RE_JAVA/src/udmy/act9/SimpleCalculatorRunner.java
@@ -0,0 +1,19 @@
+package udmy.act9;
+
+import java.math.BigDecimal;
+
+public class SimpleCalculatorRunner {
+	public static void main(String[] args) {
+//		이자율 계산
+//		 - 원금 4500.0
+//		 - 이자율 7.5%
+//		 - 대출기간 5년
+//		 > 대출기간이 끝나고 전체 이익은 얼마일지 구하는
+
+		SimpleCalculator calcultor = new SimpleCalculator("4500.00", "7.5");
+		BigDecimal totalValue = calcultor.calculateTotal(5); // 5년
+		System.out.println("총합: "+totalValue);
+		//총합: 6187.50000
+
+	}
+}
